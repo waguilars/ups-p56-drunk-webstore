a81f84c70ddc9b9f69972a8ee1328380cbd6596b
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-09 13:56:23 -0500

    register login home slider some changes

--- frontend/src/app/components/slider/slider.component.ts
+++ frontend/src/app/components/slider/slider.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SliderComponent implements OnInit {
+  @Input() showCaption: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.showCaption = true;
   }
 
+  ngOnInit(): void {}
 }
