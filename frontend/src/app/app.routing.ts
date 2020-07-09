41e8fa00a915257ce178999a2afa2a526d44f6f1
GabrielCacuango07 <gabrielcacuango07@gmail.com>
2020-07-09 10:26:06 -0500

    product detail

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LicoresComponent } from './components/licores/licores.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'licores:id', component: LicoresComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
