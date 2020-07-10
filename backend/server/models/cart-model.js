f37255e3b726de5c75d59d9423f0284f59b10e05
rromom <rromom@est.ups.edu.ec>
2020-07-09 19:26:52 -0500

    arreglando un bug :v

--- backend/server/models/cart-model.js
+++ backend/server/models/cart-model.js
@@ -4,23 +4,23 @@ const categoryModel = require('./category-model');
 const Schema = mongoose.Schema;
 
 const cartItem = {
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: 'Producto',
-    required: [true, 'No se ha seleccionado ningun producto'],
-  },
-  quantity: {
-    type: Number,
-    default: 0,
-  },
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: 'Producto',
+        required: [true, 'No se ha seleccionado ningun producto'],
+    },
+    quantity: {
+        type: Number,
+        default: 0,
+    },
 };
 
 const cartSchema = new Schema({
-  items: [cartItem],
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
+    items: [cartItem],
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
 });
 
-module.exports = mongoose.model('Carrito', categoryModel);
+module.exports = mongoose.model('Carrito', cartSchema);
--- backend/server/routes/product-routes.js
+++ backend/server/routes/product-routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+const product_controller = require('../controllers/product-controller');
+
 
 
 module.exports = router;
