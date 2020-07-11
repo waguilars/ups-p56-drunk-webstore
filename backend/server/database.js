f2d71347a7b00f44306a0ca70c2c75801cbfb59c
rromom <rromom@est.ups.edu.ec>
2020-07-11 16:56:29 -0500

    conexion con mongo atlas online a traves de enviroment

--- backend/server/database.js
+++ backend/server/database.js
@@ -4,7 +4,7 @@ const URI = 'mongodb://localhost/data-licores';
 
 // const URI2 = 'mongodb+srv://MongoUser:<Password>@cluster0.wbaqo.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
-mongoose.connect(URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true })
     .then(console.log('DB is connexted'))
     .catch(err => console.error(err))
 
