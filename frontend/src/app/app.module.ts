9e6e42bfd4a52344b4c91e33ca44d46767fe8288
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-13 12:13:57 -0500

    frontend - login form validated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AdminModule } from './admin/admin.module';
 
 // Components
 import { LicoresComponent } from './components/licores/licores.component';
-import { LoginComponent } from './components/login/login.component';
+
 import { RegisterComponent } from './components/register/register.component';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
@@ -14,11 +15,12 @@ import { FooterHomeComponent } from './shared/footer-home/footer-home.component'
 import { SliderComponent } from './components/slider/slider.component';
 import { NavbarHomeComponent } from './shared/navbar-home/navbar-home.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
 
 // Routing
 import { AppRoutingModule } from './app.routing';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
-import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,14 @@ import { CartComponent } from './components/cart/cart.component';
     NotFoundComponent,
     CartComponent,
   ],
-  imports: [BrowserModule, AdminModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AdminModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
