60db882a285085f4b2f018e5685c48531539a62c
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-13 19:40:49 -0500

    frontend proxy to connect with the api

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  apiURI = environment.api + '/user';
+  constructor(private http: HttpClient) {
+    console.log(this.apiURI);
+  }
+
+  register(): Observable<any> {
+    return this.http.post(`${this.apiURI}/registro`, null);
+  }
+}
