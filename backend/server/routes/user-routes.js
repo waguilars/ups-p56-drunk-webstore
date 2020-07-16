9c19c32bc7a735f8e9882d6a0a2a9c388bf46f1d
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-15 19:16:22 -0500

    change routes

--- backend/server/routes/user-routes.js
+++ backend/server/routes/user-routes.js
@@ -6,17 +6,18 @@ const { checkToken } = require('../middlewares/auth');
 
 // Registrar un usuario
 router.post(
-    '/registro', [v.name, v.lastname, v.email, v.password],
-    user_controller.insertUser
+  '/registro',
+  [v.name, v.lastname, v.email, v.password],
+  user_controller.insertUser
 );
 
 router.post('/login', user_controller.loginUser);
 
 router.post('/auth', [checkToken], user_controller.logedIn);
 
-router.get('/list', user_controller.getUsers);
+router.get('/', user_controller.getUsers);
 
-router.get('/user/:id', user_controller.getUser);
+router.get('/:id', user_controller.getUser);
 
 router.delete('/block/:id', user_controller.deleteUser);
 
