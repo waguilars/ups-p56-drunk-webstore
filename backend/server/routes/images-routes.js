90885b6c3b3020a3c3445557c1d3c2f73784ce89
GabrielCacuango07 <gabrielcacuango07@gmail.com>
2020-07-16 20:41:14 -0500

    Ingreso imagen de producto

--- backend/server/routes/images-routes.js
+++ backend/server/routes/images-routes.js
@@ -6,5 +6,6 @@ const { onlyImages } = require('../middlewares/upload');
 const uploadCtrl = require('../controllers/images-controller');
 
 router.put('/user', [checkToken, onlyImages], uploadCtrl.uploadUser);
+router.put('/product/:id', [checkToken, onlyImages], uploadCtrl.uploadProduct);
 
 module.exports = router;
