1dfeec3a7aeb9b63ec09ff490c3cbd1441208bb4
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-17 18:34:26 -0500

    login fix bug

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,35 +2,47 @@ import { Injectable, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
-import { UserModel } from '../models/user.model';
+import {
+  UserModel,
+  ResponseLogin,
+  ResponseValidSesion,
+  SessionData,
+} from '../models/user.model';
 
-import { Observable, throwError, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   apiURI = environment.api + '/user';
+  user: UserModel;
 
-  @Output() getAuthUser: EventEmitter<any>;
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-    this.getAuthUser = new EventEmitter();
+  signIn(user: UserModel): Observable<UserModel> {
+    return this.http.post(`${this.apiURI}/login`, user).pipe(
+      map((res: ResponseLogin) => {
+        this.user = UserModel.getInstance(res.user);
+        localStorage.setItem('token', res.token);
+        return this.user;
+      })
+    );
+  }
+
+  logout(): boolean {
+    const token = localStorage.getItem('token');
+    this.user = null;
+    if (token) {
+      localStorage.removeItem('token');
+      return true;
+    }
+    return false;
   }
 
   register(newUser: UserModel): Observable<any> {
     return this.http.post(`${this.apiURI}/registro`, newUser);
-  }
-
-  login(data: UserModel): Observable<any> {
-    return this.http.post(`${this.apiURI}/login`, data).pipe(
-      map((res: any) => {
-        this.saveToken(res.token);
-        this.getAuthUser.emit(res.user);
-        return res;
-      })
-    );
   }
 
   get(id: string): Observable<UserModel | any> {
@@ -59,26 +71,16 @@ export class UserService {
     );
   }
 
-  logout(): void {
-    this.getAuthUser.error('logout');
-    localStorage.removeItem('token');
-  }
-
-  saveToken(token: string): void {
-    localStorage.setItem('token', token);
+  isAuth(): Observable<SessionData> {
+    return this.http.post(`${this.apiURI}/auth`, null).pipe(
+      map((res: ResponseValidSesion) => {
+        this.user = res.data.user;
+        return res.data;
+      })
+    );
   }
 
   getToken(): string {
     return localStorage.getItem('token');
-  }
-
-  isAuth(): Observable<any> {
-    return this.http.post(`${this.apiURI}/auth`, null).pipe(
-      map((res: any) => {
-        // console.log(res);
-        this.getAuthUser.emit(res.data.user);
-        return res;
-      })
-    );
   }
 }
