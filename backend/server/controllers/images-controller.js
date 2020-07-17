817e2d065af14f501dd749471e5a0ad959cd2619
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-17 09:34:38 -0500

    correction upload img

--- backend/server/controllers/images-controller.js
+++ backend/server/controllers/images-controller.js
@@ -5,160 +5,152 @@ const fs = require('fs');
 
 const ctrl = {};
 
-ctrl.uploadUser = async(req, res) => {
-    const user = req.user.user;
-    let image = req.img;
-    const fileName = `${user._id}-${new Date().getMilliseconds()}.${image.ext}`;
+ctrl.uploadUser = async (req, res) => {
+  const user = req.user.user;
+  let image = req.img;
+  const fileName = `${user._id}-${new Date().getMilliseconds()}.${image.ext}`;
 
-    image = image.file;
-    const outDir = path.resolve(
-        __dirname,
-        `../../uploads/images/users/${fileName}`
-    );
+  image = image.file;
+  const outDir = path.resolve(
+    __dirname,
+    `../../uploads/images/users/${fileName}`
+  );
 
-    image.mv(outDir, (err) => {
-        if (err)
-            return res.status(500).json({
-                status: false,
-                err,
-            });
+  image.mv(outDir, (err) => {
+    if (err)
+      return res.status(500).json({
+        status: false,
+        err,
+      });
 
-        updateUserImg(res, user._id, fileName);
-    });
+    updateUserImg(res, user._id, fileName);
+  });
 };
 
+ctrl.uploadProduct = async (req, res) => {
+  const prod = req.params.id;
+  let image = req.img;
+  // cambia el nombre del archivo a uno unico
+  const fileName = `${prod}-${new Date().getMilliseconds()}.${image.ext}`;
+  image = image.file;
+  // console.log(image);
+  const outDir = path.resolve(
+    __dirname,
+    `../../uploads/images/products/${fileName}`
+  );
+  image.mv(outDir, (err) => {
+    if (err)
+      return res.status(500).json({
+        status: false,
+        err,
+      });
 
-ctrl.uploadProduct = async(req, res) => {
-
-    const prod = req.params.id;
-    let image = req.img;
-    // cambia el nombre del archivo a uno unico
-    const fileName = `${prod}-${new Date().getMilliseconds()}.${image.ext}`
-    image = image.file;
-    console.log(image);
-    const outDir = path.resolve(
-        __dirname,
-        `../../uploads/images/products/${fileName}`
-    );
-    image.mv(outDir, (err) => {
-        if (err)
-            return res.status(500).json({
-                status: false,
-                err,
-            });
-
-        updateProductImg(res, prod, fileName);
-    });
+    updateProductImg(res, prod, fileName);
+  });
 };
-// desde aqui la falla del Gabriel  
+// desde aqui la falla del Gabriel
 
 const updateProductImg = (response, productID, imgName) => {
-    Product.findOne({ _id: productID }, (err, productDB) => {
-        if (err) {
-            deleteImg(imgName, 'products');
-            return response.status(500).json({
-                status: false,
-                err: {
-                    msg: 'Error en la peticion.',
-                    err
-                },
-            });
-        }
+  Product.findOne({ _id: productID }, (err, productDB) => {
+    if (err) {
+      deleteImg(imgName, 'products');
+      return response.status(500).json({
+        status: false,
+        err: {
+          msg: 'Error en la peticion.',
+        },
+      });
+    }
 
-        if (!productDB) {
-            deleteImg(imgName, 'products');
-            return response.status(404).json({
-                status: false,
-                err: {
-                    msg: 'El producto no existe.',
-                },
-            });
-        }
+    if (!productDB) {
+      deleteImg(imgName, 'products');
+      return response.status(404).json({
+        status: false,
+        err: {
+          msg: 'El producto no existe.',
+        },
+      });
+    }
 
+    if (productDB.img) {
+      deleteImg(productDB.img, 'products');
+    }
 
-        if (productDB.img) {
-
-            deleteImg(procutDB.img, 'products');
-        }
-
-        productDB.img = imgName;
-        productDB.save((err, saved) => {
-            if (err) {
-                deleteImg(productDB.img, 'products');
-                return response.status(500).json({
-                    status: false,
-                    err: {
-                        msg: 'Error en la peticion.',
-                    },
-                });
-            }
-
-            return response.json({
-                status: true,
-                product: saved,
-            });
+    productDB.img = imgName;
+    productDB.save((err, saved) => {
+      if (err) {
+        deleteImg(productDB.img, 'products');
+        return response.status(500).json({
+          status: false,
+          err: {
+            msg: 'Error en la peticion.',
+          },
         });
+      }
+
+      return response.json({
+        status: true,
+        product: saved,
+      });
     });
+  });
 };
 
-
 const updateUserImg = (response, userID, imgName) => {
-    User.findOne({ _id: userID }, (err, userDB) => {
-        if (err) {
-            deleteImg(imgName, 'users');
-            return response.status(500).json({
-                status: false,
-                err: {
-                    msg: 'Error en la peticion.',
-                },
-            });
-        }
+  User.findOne({ _id: userID }, (err, userDB) => {
+    if (err) {
+      deleteImg(imgName, 'users');
+      return response.status(500).json({
+        status: false,
+        err: {
+          msg: 'Error en la peticion.',
+        },
+      });
+    }
 
-        if (!userDB) {
-            deleteImg(imgName, 'users');
-            return response.status(404).json({
-                status: false,
-                err: {
-                    msg: 'El usuario no existe.',
-                },
-            });
-        }
+    if (!userDB) {
+      deleteImg(imgName, 'users');
+      return response.status(404).json({
+        status: false,
+        err: {
+          msg: 'El usuario no existe.',
+        },
+      });
+    }
 
-        if (userDB.img) {
+    if (userDB.img) {
+      deleteImg(userDB.img, 'users');
+    }
 
-            deleteImg(userDB.img, 'users');
-        }
-
-
-        userDB.img = imgName;
-        userDB.save((err, saved) => {
-            if (err) {
-                deleteImg(userDB.img, 'users');
-                return response.status(500).json({
-                    status: false,
-                    err: {
-                        msg: 'Error en la peticion.',
-                    },
-                });
-            }
-
-            return response.json({
-                status: true,
-                user: saved,
-            });
+    userDB.img = imgName;
+    userDB.save((err, saved) => {
+      if (err) {
+        deleteImg(userDB.img, 'users');
+        return response.status(500).json({
+          status: false,
+          err: {
+            msg: 'Error en la peticion.',
+          },
         });
+      }
+
+      return response.json({
+        status: true,
+        user: saved,
+      });
     });
+  });
 };
 
 const deleteImg = (filename, type) => {
-    const pathimg = path.resolve(
-        __dirname,
-        `../../uploads/images/${type}/${filename}`
-    );
+  const pathimg = path.resolve(
+    __dirname,
+    `../../uploads/images/${type}/${filename}`
+  );
 
-    if (fs.existsSync(pathimg)) {
-        fs.unlinkSync(pathimg);
-    }
+  if (fs.existsSync(pathimg)) {
+    fs.unlinkSync(pathimg);
+  }
 };
 
 module.exports = ctrl;
--- backend/server/controllers/product-controller.js
+++ backend/server/controllers/product-controller.js
@@ -5,112 +5,111 @@ const { get } = require('mongoose');
 const prodCtr = {};
 
 let order_text = (texto) => {
-    let palabras = texto.split(' ');
-    palabras.forEach((palabra, idx) => {
-        palabras[idx] =
-            palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
-    });
-    return palabras.join(' ');
+  let palabras = texto.split(' ');
+  palabras.forEach((palabra, idx) => {
+    palabras[idx] =
+      palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+  });
+  return palabras.join(' ');
 };
 
 let order_text_description = (texto) => {
-    let palabras = texto.split(' ');
-    palabras.forEach((palabra, idx) => {
-        if (idx === 0) {
-            palabras[idx] =
-                palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
-        } else {
-            palabras[idx] =
-                palabra.charAt(0).toLowerCase() + palabra.slice(1).toLowerCase();
-        }
-    });
-    return palabras.join(' ');
+  let palabras = texto.split(' ');
+  palabras.forEach((palabra, idx) => {
+    if (idx === 0) {
+      palabras[idx] =
+        palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    } else {
+      palabras[idx] =
+        palabra.charAt(0).toLowerCase() + palabra.slice(1).toLowerCase();
+    }
+  });
+  return palabras.join(' ');
 };
 
 prodCtr.insertProduct = (req, res) => {
-    let mistakes = validationResult(req)
-    if (mistakes.isEmpty()) {
-        let body = req.body;
-        let param = {
-            name: order_text(body.name),
-            desc_short: order_text_description(body.desc_short),
-            desc_long: order_text_description(body.desc_long),
-            price: body.price,
-            category: body.category,
-            img: body.img,
-            stock: body.stock
-        }
-        product_model.find({}, 'name', (err, data) => {
-            if (err) {
-                return res.status(500).json({
-                    status: false,
-                    error: err
-                });
-            }
-            let flag = data.find(prod => prod.name === param.name);
-            if (flag) {
-                return res.status(400).json({
-                    status: false,
-                    msg: "Este producto ya se encuentra registrado "
-                });
-            };
-            let new_prod = new product_model(param);
-            new_prod.save((err, data) => {
-                if (err) {
-                    return res.status(500).json({
-                        status: false,
-                        error: err
-                    });
-                }
-                res.status(201).json({
-                    status: true,
-                    msg: "Producto registrado",
-                    product: data
-                });
-            });
-        })
-    } else {
-        let new_err = {};
-        mistakes.errors.forEach(element => {
-            new_err[element.param] = element.msg;
+  let mistakes = validationResult(req);
+  if (mistakes.isEmpty()) {
+    let body = req.body;
+    let param = {
+      name: order_text(body.name),
+      desc_short: order_text_description(body.desc_short),
+      desc_long: order_text_description(body.desc_long),
+      price: body.price,
+      category: body.category,
+      img: body.img,
+      stock: body.stock,
+    };
+    product_model.find({}, 'name', (err, data) => {
+      if (err) {
+        return res.status(500).json({
+          status: false,
+          error: err,
         });
+      }
+      let flag = data.find((prod) => prod.name === param.name);
+      if (flag) {
         return res.status(400).json({
-            status: false,
-            msg: "Parametros no admitidos",
-            error: new_err
+          status: false,
+          msg: 'Este producto ya se encuentra registrado ',
         });
-    }
+      }
+      let new_prod = new product_model(param);
+      new_prod.save((err, data) => {
+        if (err) {
+          return res.status(500).json({
+            status: false,
+            error: err,
+          });
+        }
+        res.status(201).json({
+          status: true,
+          msg: 'Producto registrado',
+          product: data,
+        });
+      });
+    });
+  } else {
+    let new_err = {};
+    mistakes.errors.forEach((element) => {
+      new_err[element.param] = element.msg;
+    });
+    return res.status(400).json({
+      status: false,
+      msg: 'Parametros no admitidos',
+      error: new_err,
+    });
+  }
 };
 
 prodCtr.getProducts = (req, res) => {
-    product_model.find({}, (err, data) => {
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                msg: err
-            });
-        }
-        res.status(201).json({
-            status: true,
-            product: data
-        });
+  product_model.find({}, (err, data) => {
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        msg: err,
+      });
+    }
+    res.status(201).json({
+      status: true,
+      product: data,
     });
-}
-
+  });
+};
 
 prodCtr.getproduct = (req, res) => {
-    let id = req.params.id;
-    product_model.findById(id, (err, data) => {
-        if (err) {
-            return res.status(404).json({
-                status: false,
-                msg: 'producto no encontrado'
-            });
-        }
-        res.status(200).json({
-            status: true,
-            product: data
-        });
-    })
+  let id = req.params.id;
+  product_model.findById(id, (err, data) => {
+    if (err) {
+      return res.status(404).json({
+        status: false,
+        msg: 'producto no encontrado',
+      });
+    }
+    res.status(200).json({
+      status: true,
+      product: data,
+    });
+  });
 };
 module.exports = prodCtr;
