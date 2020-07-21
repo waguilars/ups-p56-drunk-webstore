81146bbd368510e95239f717fc5d9934aa5c8a8e
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-20 22:47:06 -0500

    api add card only new

--- backend/server/models/cart-model.js
+++ backend/server/models/cart-model.js
@@ -4,29 +4,32 @@ const { schema } = require('./category-model');
 
 const Schema = mongoose.Schema;
 
-const cartItem = {
+const cartItem = new Schema(
+  {
     product: {
-        type: Schema.Types.ObjectId,
-        ref: 'Producto',
-        required: [true, 'No se ha seleccionado ningun producto'],
+      type: Schema.Types.ObjectId,
+      ref: 'Producto',
+      required: [true, 'No se ha seleccionado ningun producto'],
     },
     quantity: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
-};
+  },
+  { _id: false }
+);
 
 const cartSchema = new Schema({
-    items: [cartItem],
-    totalPrice: {
-        type: Number,
-        default: 0
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'Usuario',
-        require: true
-    }
+  items: [cartItem],
+  totalPrice: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario',
+    require: true,
+  },
 });
 
 module.exports = mongoose.model('Carrito', cartSchema);
