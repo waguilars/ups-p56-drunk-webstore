2976c5dc1f22854db09880eb6ff5b29b7cd0a0d8
rromom <rromom@est.ups.edu.ec>
2020-07-21 18:52:32 -0500

    insert and update cart

--- backend/server/controllers/cart-controller.js
+++ backend/server/controllers/cart-controller.js
@@ -1,85 +1,120 @@
-const express = require('express');
 const cart_model = require('../models/cart-model');
 const { validationResult } = require('express-validator');
 const product_model = require('../models/product-model');
 
 const cartCTR = {};
 
-cartCTR.insertCart = (req, res) => {
-  let mistakes = validationResult(req);
-  if (!mistakes.isEmpty()) {
-    let new_err = {};
-    mistakes.errors.forEach((element) => {
-      let param = element.param;
-      let msg_param = element.msg;
-      new_err[param] = msg_param;
-    });
-    return res.status(400).json({
-      status: false,
-      msg: 'Parametros mal enviados',
-      error: new_err,
-    });
-  }
-  product_model.findById(req.params.id, (err, producto) => {
-    if (err) {
-      return res.status(500).json({
-        status: false,
-        msg: 'Error en el servidor',
-        err,
-      });
-    }
-    if (!producto) {
-      return res.status(404).json({
-        status: false,
-        msg: 'Producto no encontrado',
-        err,
-      });
-    }
-
-    cart_model
-      .findOne({ user: req.user.user._id })
-      .populate('user')
-      .populate('items.product', 'name price category')
-      .exec((err, cart) => {
-        if (err) {
-          return res.status(500).json({
+cartCTR.insertAndUpdateCart = (req, res) => {
+    let mistakes = validationResult(req);
+    if (!(mistakes.isEmpty())) {
+        let new_err = {};
+        mistakes.errors.forEach((element) => {
+            let param = element.param;
+            let msg_param = element.msg;
+            new_err[param] = msg_param;
+        });
+        return res.status(400).json({
             status: false,
-            msg: 'Error en el servidor',
-            err,
-          });
-        }
-
-        if (!cart) {
-          let product = {
-            product: req.params.id,
-            quantity: req.body.quantity,
-          };
-          let new_cart = new cart_model({
-            items: [product],
-            totalPrice: req.body.quantity * producto.price,
-            user: req.user.user._id,
-          });
-
-          new_cart.save((err, cart_new) => {
-            if (err) {
-              return res.status(500).json({
+            msg: 'Parametros mal enviados',
+            error: new_err,
+        });
+    }
+    let id_producto = req.params.id
+    product_model.findById(id_producto, (err, producto) => {
+        if (err) {
+            return res.status(500).json({
                 status: false,
-                msg: 'Error en el servidor',
-                err,
-              });
-            }
-            return res.status(200).json({
-              msg: 'Crear cart',
-              cart_new,
+                msg: "Error en el servidor",
+                err
             });
-          });
-        } else {
-          return res.json({
-            cart,
-          });
         }
-      });
-  });
+        if (!producto) {
+            return res.status(404).json({
+                status: false,
+                msg: "Producto no encontrado",
+                err
+            });
+        }
+        if (producto.stock < req.body.quantity) {
+            return res.status(400).json({
+                status: false,
+                msg: "No hay productos suficientes para cumplir con su pedido",
+                err
+
+            });
+        }
+        cart_model
+            .findOne({ user: req.user.user._id })
+            .populate('user')
+            .populate('items.product', 'name price category')
+            .exec((err, cart) => {
+                if (err) {
+                    return res.status(500).json({
+                        status: false,
+                        msg: "Error en el servidor",
+                        err
+                    });
+                }
+                let product = {
+                    product: req.params.id,
+                    quantity: req.body.quantity,
+                };
+                if (!cart) {
+
+                    let new_cart = new cart_model({
+                        items: [product],
+                        totalPrice: req.body.quantity * producto.price,
+                        user: req.user.user._id,
+                    });
+
+                    new_cart.save((err, cart_new) => {
+                        if (err) {
+                            return res.status(500).json({
+                                status: false,
+                                msg: 'Error en el servidor',
+                                err,
+                            });
+                        }
+                        return res.status(200).json({
+                            msg: 'Crear cart',
+                            cart_new,
+                        });
+                    });
+                } else {
+                    let caritems = cart.items;
+                    // console.log(caritems);
+                    let flag = caritems.findIndex(car_product =>
+                        `${car_product.product._id}` === `${req.params.id}`);
+                    if (flag >= 0) {
+                        let quantity = parseInt(cart.items[flag].quantity) + parseInt(req.body.quantity);
+                        cart.items[flag].quantity = quantity;
+                    } else {
+                        cart.items.push(product);
+                    }
+
+                    let totalPrice = parseFloat(cart.totalPrice);
+                    totalPrice += (req.body.quantity * producto.price);
+                    cart.totalPrice = totalPrice;
+                    cart_model.findByIdAndUpdate(cart._id, cart, { new: true }, (err, guardado) => {
+                        if (err) {
+                            return res.status(500).json({
+                                status: false,
+                                msg: 'Error en el servidor',
+                                err,
+                            });
+                        }
+                        res.status(200).json({
+                            status: true,
+                            guardado
+
+                        });
+                    });
+                }
+
+            });
+
+    });
 };
+
 
 module.exports = cartCTR;
--- backend/server/routes/cart-routes.js
+++ backend/server/routes/cart-routes.js
@@ -3,6 +3,8 @@ const router = Router();
 const cart_controller = require('../controllers/cart-controller');
 const { checkToken } = require('../middlewares/auth');
 const v = require('../helpers/validation');
-router.post('/:id', [checkToken, v.param_id, v.quantity], cart_controller.insertCart);
+
+router.post('/:id', [checkToken, v.param_id, v.quantity], cart_controller.insertAndUpdateCart)
+
 
 module.exports = router;
