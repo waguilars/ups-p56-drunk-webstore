f749710fbec4325f887b71f9256b2a7bcd472843
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-23 07:30:53 -0500

    cart page without img

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  cart: Cart;
+  constructor(private cartSv: CartService) {
+    this.cart = null;
   }
 
+  ngOnInit(): void {
+    this.getCart();
+  }
+
+  getCart(): void {
+    this.cartSv.getCart().subscribe((res) => (this.cart = res));
+  }
 }
--- frontend/src/app/models/cart.model.ts
+++ frontend/src/app/models/cart.model.ts
@@ -14,6 +14,7 @@ export interface Cart {
   items: Item[];
   totalItems: number;
   user: UserModel;
+  total: number;
 }
 
 export interface Item {
