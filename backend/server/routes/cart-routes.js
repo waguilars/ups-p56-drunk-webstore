36ed9e0f87fa9b1cba1cfdbcab1554164f3a6a13
rromom <rromom@est.ups.edu.ec>
2020-07-23 13:29:16 -0500

    delete carrito

--- backend/server/routes/cart-routes.js
+++ backend/server/routes/cart-routes.js
@@ -4,10 +4,10 @@ const cart_controller = require('../controllers/cart-controller');
 const { checkToken } = require('../middlewares/auth');
 const v = require('../helpers/validation');
 
-router.post('/:id', [checkToken, v.param_id, v.quantity], cart_controller.insertAndUpdateCart)
+router.post('/:id', [checkToken, v.param_id, v.quantity], cart_controller.insertAndUpdateCart);
 
-router.get('/', [checkToken], cart_controller.getCart)
+router.get('/', [checkToken], cart_controller.getCart);
 
-
+router.delete('/', [checkToken], cart_controller.emptyCart);
 
 module.exports = router;
