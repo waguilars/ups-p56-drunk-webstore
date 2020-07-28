4a60130c915890463f569970141db132a3a4cc01
rromom <rromom@est.ups.edu.ec>
2020-07-28 18:58:14 -0500

    matriz user id product id and queantity

--- recommended-system/app.py
+++ recommended-system/app.py
@@ -4,6 +4,8 @@ from flask_pymongo import PyMongo
 from bson import json_util
 from bson.objectid import ObjectId
 from sklearn.metrics.pairwise import pairwise_distances,cosine_distances
+import numpy as np
+from sklearn.decomposition import TruncatedSVD
 
 from decouple import config as config_decouple
 from config import config
@@ -46,33 +48,84 @@ def recommended_products(id):
         return invalidObjectId()
 
     id = ObjectId(id)
-    # print(id)
+    # # print(id)
     prods = mongo.db.productos.find()
     prods = [str(prod['_id']) for prod in prods]
 
-    users = mongo.db.usuarios.find()
-    users = [str(user['_id']) for user in users]
+    # users = mongo.db.usuarios.find()
+    # users = [str(user['_id']) for user in users]
 
-    mtx = pd.DataFrame(int(0), columns=users, index=prods)
-    # print(utility_mtx)
-    sales = mongo.db.ordens.find()
-    for sale in sales:
-        user_id = str(sale['user'])
-        data = sale['cart']
-        data = data['products']
-        for prod in data:
-            prod_id = str(prod['_id'])
-            quantity = prod['quantity']
-            mtx[user_id][prod_id] = mtx[user_id][prod_id] + quantity
-    ### MATRIZ SIMILITUD
-    ### JACKARD
-    jac_sim = 1 - pairwise_distances(mtx.T, metric = "hamming")
-    jac_sim = pd.DataFrame(jac_sim, index=users, columns=users)
-    ### COSENO
-    sim_matrix = 1 - cosine_distances(mtx.T)
-    sim_matrix = pd.DataFrame(sim_matrix, index=users, columns=users)
-    print(sim_matrix)
+    ordens = mongo.db.ordens.find()
+    orden = []
+    for order in ordens:
+        for cart,cart_value in  order.get('cart').items():
+            for products in cart_value:
+                prod=[]
+                for product,product_value in products.items():
+                    prod.append(product_value)
+                # print(prod)
+                orden.append([str(order['user']),str(prod[0]),prod[4]])
 
+                # print("------------------------")
+            break
+
+    mtx3=pd.DataFrame(orden)
+    mtx3.columns = ['user','product','quantity']
+    print(mtx3)
+
+    # users_ids=[ i[0] for i in orden ]
+    # users_ids=list(set(users_ids))
+    # mtx2 = pd.DataFrame(int(0), index=users_ids, columns=prods)
+
+    # for i  in orden:
+    #     mtx2._set_value(i[0],i[1],(int(i[2]+mtx2._get_value(i[0],i[1]))))
+    
+    # mtx_sim = pairwise_distances(mtx2,metric='euclidean')
+    # print(mtx_sim.shape)
+    # # mtx2.loc['5f164c956443183c2ceae540','5f14d5cc86d0bd2a08a3e60d']=1
+    # # print(mtx2.loc['5f164c956443183c2ceae540','5f14d5cc86d0bd2a08a3e60d'])
+
+
+    # mtx = pd.DataFrame(int(0), columns=users, index=prods)
+    # # print(utility_mtx)
+    # sales = mongo.db.ordens.find()
+    # for sale in sales:
+    #     user_id = str(sale['user'])
+    #     data = sale['cart']
+    #     data = data['products']
+    #     for prod in data:
+    #         prod_id = str(prod['_id'])
+    #         quantity = prod['quantity']
+    #         mtx[user_id][prod_id] = 1
+    
+
+
+    # jac_sim = 1 - pairwise_distances(mtx.T, metric = "hamming") 
+    # jac_sim = pd.DataFrame(jac_sim, index=users, columns=users)
+    # user=jac_sim[str(id)]
+    # user1=[i for i in user if i!=1]
+    # maximo=np.amax(user1)
+    # usurio_coincidencia=''
+    # for key,i in user.items():
+    #     if (i == maximo):
+    #         usurio_coincidencia=key
+    #         break
+    # print(user)
+    # print(maximo)
+    # products_user1 = mtx[str(id)].to_numpy()
+    # print(products_user1)
+    # products_user2 = mtx[str(usurio_coincidencia)].to_numpy()
+    # print(products_user2)
+    # print(usurio_coincidencia)
+    # for key, value in products_user1.items():
+    #     print(products_user2[key])
+    #     print(products_user1[key])
+    #     print('\n')
+    
+
+    # only2 = products_user2[products_user2.isin(products_user1)==False]
+    # print(products_user1)
+    
     return Response(['prods'], mimetype='application/json')
 
 
